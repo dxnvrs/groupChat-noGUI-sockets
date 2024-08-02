568b7807efbaabc71c4dbf0d6036fba8c80d0ae8
dan <146361955+dxnvrs@users.noreply.github.com>
2024-08-01 23:41:27 -0300

    Update client-noGUI.py

--- client-noGUI.py
+++ client-noGUI.py
@@ -1,31 +1,16 @@
+# all libs used
 import socket
 import threading
 import time
 import os
 
-def sendMsg():
+#######################################################################################################################################################################################
+#                                                                                                                                                                                     #
+#                                                          ****************** FUNCTIONS USED *******************                                                                      #
+#                                                                                                                                                                                     #
+#######################################################################################################################################################################################
 
-    while True:
-
-        mensagem = input()
-        
-        if mensagem.startswith('/file'):
-
-            _, target, filePath = mensagem.split(' ', 2)
-            
-            with open(filePath, 'rb') as f:
-
-                fileData = f.read()
-
-            fileName = os.path.basename(filePath)
-            client.send(f'/file {target} {fileName}'.encode())
-            client.send(str(len(fileData)).encode())
-            client.send(mensagem.encode())
-
-        else:
-
-            client.send(mensagem.encode())
-
+# main function, handles messages from server  
 def recvMsg():
 
     while True:
@@ -50,12 +35,6 @@ def recvMsg():
 
                 print(f'\n[FILE]>> arquivo {fileName} recebido\n')
 
-            elif msg == '/quit':
-
-                print('\n[DESCONECTADO]>> você se desconectou do servidor\n')
-                time.sleep(5)
-                exit()
-
             else:
 
                 print(msg)
@@ -67,35 +46,56 @@ def recvMsg():
 
     return msg
 
+#######################################################################################################################################################################################
+#                                                                                                                                                                                     #
+#                                                          ****************** CODE STARTS HERE *******************                                                                    #
+#                                                                                                                                                                                     #
+#######################################################################################################################################################################################
+
+# the host and port choosed (3030 as port 'cause ports of minor values may being used by the computer) ('127.0.0.1' is the localhost's address)
 host = '127.0.0.1'
 port = 3030
 
+# client is intanced
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
 
+    # client tries to connect to server
     client.connect((host, port))
     
+    # if it makes it, the user have to write their email and name 
     emailUser = input('\n[LOGIN]>> digite seu email:\n')
     print('>> ', emailUser)
     nameUser = input('\n[LOGIN]>> agora, digite seu nome para continuar:\n')
     print('>> ', nameUser)
 
+    # the information is sent to server
     client.send(emailUser.encode())
     client.send(nameUser.encode())
 
     print('\n[CONECTADO]>> o cliente está conectado\n')
 
 except Exception:
-
+    
+    # if it didn't work, client is closed 
     print('\n[ERRO]>> não foi possível conectar o host, pois o servidor está offline.\n')
     time.sleep(5)
     exit()
 
+# start main loop
 while True:
 
+    # threading the main function (recvMsg) and starting the process
     process = threading.Thread(target=recvMsg)
     process.start()
 
+    # all messages now are sent to server
     msg = input()
     client.send(msg.encode())
+    
+    if msg == '/quit':
+
+        print('\n[DESCONECTADO]>> você se desconectou do servidor\n')
+        time.sleep(5)
+        exit()
